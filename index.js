b6853270fdbedfb8cf10d41e990381e79482753f
AdrianNicolenco <wtf.pl.lol@gmail.com>
2022-02-12 09:44:53 -0600

    Init Database

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+
+const db = require("./models");
 /* ----- Init Server ----- */
 const app = express();
 
@@ -13,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended:false}));
-
+db.sequelize.sync();
 /* ----- Run the Server ----- */
-const PORT = process.env.API_PORT || 5000;
+const PORT = process.env.API_PORT || 3001;
 
 app.listen(PORT, ()=> {
     console.log(`Server is Running on port ${PORT}`);
