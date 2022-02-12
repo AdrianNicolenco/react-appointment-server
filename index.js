02776ed639377f7029023de42763c75488552bff
AdrianNicolenco <wtf.pl.lol@gmail.com>
2022-02-12 09:37:24 -0600

    init server 2

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+/* ----- Import Module ----- */
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+/* ----- Init Server ----- */
 const app = express();
 
+/* ----- Use Modules ---- */
 app.use(cors());
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-const PORT = process.env.PORT || 5000;
+/* ----- Run the Server ----- */
+const PORT = process.env.API_PORT || 5000;
 
 app.listen(PORT, ()=> {
     console.log(`Server is Running on port ${PORT}`);
